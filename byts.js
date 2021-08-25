ea6b33866437c0fa619f1c38803944136676cc42
Michael Dinger <31929953+Dinger1112@users.noreply.github.com>
2021-08-24 23:06:31 -0400

    Update byts.js

--- byts.js
+++ byts.js
@@ -1,1 +1,21 @@
-console.log('test');
+var styles = `
+    #show_button {
+        color: rgb(62, 166, 255);
+        cursor: pointer;
+        font-family: Roboto, Arial, sans-serif;
+        font-size: 14px;
+        font-weight: 500;
+        letter-spacing: 0.5px;
+    }
+`
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
+
+
+let show_button = document.createElement('DIV')
+show_button.id = "show_button"
+show_button.textContent = "SHOW"
+
+let title_container = document.getElementById('title-container')
+title_container.insertBefore(show_button, title_container.childNodes[5])
